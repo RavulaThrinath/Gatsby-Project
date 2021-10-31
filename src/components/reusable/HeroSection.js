44d571abe9117a45d1f6bf5a7057b5a50fd4edc0
ThrinathRavula <thrinathravual@gmail.com>
2021-10-31 10:03:17 +0530

    Image chnged

--- src/components/reusable/HeroSection.js
+++ src/components/reusable/HeroSection.js
@@ -1,5 +1,5 @@
 import React from "react"
-import BackgroundImage from "gatsby-background-image"
+// import BackgroundImage from "gatsby-background-image"
 
 export default function HeroSection({ title, subtitle  }) {
   return (
@@ -7,9 +7,9 @@ export default function HeroSection({ title, subtitle  }) {
       <section className="home">
       <div className="max-width">
         <div className="home-content">
-          <div className="text-1">{title}</div>
-          <div className="text-2">{subtitle}</div>
-          <div class="text-3">A Web Developer</div>
+          <div className="text-1"></div>
+          <div className="text-2"></div>
+          <div class="text-3"></div>
         </div>
       </div>
       </section>
